ae43405d26d0d41398071d34b0b03a1c90517f6f
Syed Jaffry <syejaffr@acbc32c3f3af.ant.amazon.com>
2018-06-18 14:28:06 +1000

    Changed component name prefixes to LiveDemo

--- DeleteEnvironment.sh
+++ DeleteEnvironment.sh
@@ -2,27 +2,27 @@
 
 ## Aurora DB
 echo "Deleting Aurora DB Cluster.."
-aws cloudformation delete-stack --stack-name "AuroraDb2" --profile fargate
+aws cloudformation delete-stack --stack-name "LiveDemoAuroraDb" --profile fargate
 read -n 1 -s
 
 ## API Gateway
 echo "Deleting API Gateway.."
-aws cloudformation delete-stack --stack-name "APIGateway"  --profile fargate
+aws cloudformation delete-stack --stack-name "LiveDemoAPIGateway"  --profile fargate
 read -n 1 -s
 
 ## Delete Fargate Service
 echo "Deleting Fargate Service"
-aws cloudformation delete-stack --stack-name "FargateService" --profile fargate
+aws cloudformation delete-stack --stack-name "LiveDemoService" --profile fargate
 read -n 1 -s
 
 ## Delete Fargate App Infrastructure
 echo "Deleting Fargate App Infrastructure.."
-aws cloudformation delete-stack --stack-name "FargateInfrastructure"  --profile fargate
+aws cloudformation delete-stack --stack-name "LiveDemoInfrastructure"  --profile fargate
 read -n 1 -s
 
 ## Delete Fargate Networking Environment
 echo "Deleting Fargate Networking Environment.."
-aws cloudformation delete-stack --stack-name "FargateNetworking"  --profile fargate
+aws cloudformation delete-stack --stack-name "LiveDemoNetworking"  --profile fargate
 
 
 
